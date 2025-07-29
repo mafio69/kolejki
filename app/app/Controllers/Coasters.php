68547480026b6bec683da6dd7e8ebc94792e5445
Mariusz Franciszczak <mariusz.franciszczak@gmail.com>
2025-07-29 21:36:03 +0200

    Refactor: Uproszczenie walidacji przy tworzeniu kolejki
    Usunięto zbędne sprawdzanie logiki biznesowej (personel, wydajność) z metody create w kontrolerze Coasters. Walidacja ta jest niepotrzebna na tym etapie i uniemożliwiała poprawne działanie testów wydajnościowych.

--- app/app/Controllers/Coasters.php
+++ app/app/Controllers/Coasters.php
@@ -3,22 +3,16 @@
 namespace App\Controllers;
 
 use App\Models\CoasterRepository;
-use App\Services\PersonnelService;
-use App\Services\PerformanceService;
 use CodeIgniter\HTTP\ResponseInterface;
 use Config\Services;
 
 class Coasters extends BaseController
 {
     private CoasterRepository $coasterRepository;
-    private PersonnelService $personnelService;
-    private PerformanceService $performanceService;
 
     public function __construct()
     {
         $this->coasterRepository = service('coasterRepository');
-        $this->personnelService = new PersonnelService();
-        $this->performanceService = new PerformanceService();
     }
 
     /**
